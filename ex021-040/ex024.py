2e4760887e1b562c96b225260beb9e38c24e96ed
Hugo Maia <maiahugom123@gmail.com>
2025-10-16 16:15:29 -0300

    corrigindo erro no strip do ex024

--- ex021-040/ex024.py
+++ ex021-040/ex024.py
@@ -1,4 +1,4 @@
-cidade = str(input('Digite o nome de uma cidade: ').strip())
+cidade = str(input('Digite o nome de uma cidade: ')).strip()
 print(cidade[:5].upper() == "SANTO")
 # O método split() divide a string em uma lista de palavras, removendo espaços extras.
 # O método upper() converte a string para maiúsculas.
