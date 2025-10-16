3961f6c9c112020eb1fe0487d65de9df7f58b7a5
Hugo Maia <maiahugom123@gmail.com>
2025-10-16 15:47:24 -0300

    adicionando ex024 em exercicio md. Resolvendo ex024

---
+++ ex021-040/ex024.py
@@ -0,0 +1,5 @@
+cidade = str(input('Digite o nome de uma cidade: ').strip())
+print(cidade[:5].upper() == "SANTO")
+# O método split() divide a string em uma lista de palavras, removendo espaços extras.
+# O método upper() converte a string para maiúsculas.
+# A comparação é feita com "SANTO" em maiúsculas para garantir que a verificação seja case-insensitive.
